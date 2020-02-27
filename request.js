149991947e756967d156463bb10f65e51cd73878
Maineiac <webmaster@maineiac.dev>
2020-02-27 12:05:35 -0500

    create async request.get(url), shortcut for axios.

--- request.js
+++ request.js
@@ -1,6 +1,8 @@
-exports.parseError = function(error) {
+const config = require('./config.js');
+const util = require('util');
+function parseError(e) {
     var array;
-    switch(error) {
+    switch(e) {
         case 400:
             array = ['400: Bad Request', 'Invalid request. Did you modify sometihng other than the config?'];
         break;
@@ -35,5 +37,47 @@ exports.parseError = function(error) {
             array = ['Unknown error', 'Something really bad probably happened...'];
         break;
     }
-    return array;
+    result = array;
+}
+const instance = require('axios').create({
+
+    /*  This is all information that is used to make get/post resquests
+        from/to the WISP API. The documentation can be found here :
+            https://docs.panel.gg/#introduction
+        This configures axios one time to use the information to make requests.
+    */
+    baseURL: `${config.PanelURL}api/client/servers/`,
+    timeout: 1000,
+    headers: {
+
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.wisp.v1+json',
+        'Authorization': `Bearer ${config.WISPAPIKey}`
+
+    }
+
+});
+
+exports.get = async function(url) {
+    var result;
+    await instance.get(url)
+    .then(function (response) { // Successfully received a response.
+        result = response;
+    })
+    .catch(function (error) { 
+
+        if (error.response) { // Error response received.
+            result = parseError(error.response.status);
+
+        } else if (error.request) { // No response
+            result = "Couldn't find domain. Check that the URL is properly configured.";
+
+        } else {  // Something else, this seems really bad.
+            result = "Something really bad happened";
+            console.log('Error', error.message);
+
+        }
+        
+    });
+    return result;
 }
