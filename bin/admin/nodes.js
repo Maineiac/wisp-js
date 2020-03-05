c64bc306db4da67a630b8b610bebe871ef7f16f0
Maineiac <webmaster@maineiac.dev>
2020-03-04 21:37:41 -0500

    remove debugging

--- bin/admin/nodes.js
+++ bin/admin/nodes.js
@@ -3,11 +3,9 @@ exports.list = async function() {
     var result;
     await request.get(`application/nodes`, false)
     .then(function (response) { // If we got a response we send a success embed
-        //console.log(response.data)
         result = response.data.data;
 
     }).catch((error) => { // Otherwise we send an error embed
-        console.log(error)
         throw error;
 
     });
