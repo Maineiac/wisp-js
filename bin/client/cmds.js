aac8b03277e11a4c7b20316ddd82c7ec998d348a
Maineiac <webmaster@maineiac.dev>
2020-03-06 09:17:01 -0500

    handle commands listed in last commit

---
+++ bin/client/cmds.js
@@ -0,0 +1,52 @@
+const config = require('../../config.js');
+const embed = require('../embeds/servers.js');
+
+const instance = require('axios').create({
+
+    /*  This is all information that is used to make get/post resquests
+        from/to the WISP API. The documentation can be found here :
+            https://docs.panel.gg/#introduction
+        This configures axios one time to use the information to make requests.
+    */
+    baseURL: `${config.PanelURL}api/client/server/`,
+    timeout: 5000,
+    headers: {
+
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.wisp.v1+json',
+        'Authorization': `Bearer ${config.WISPAPIKey}`,
+        'User-Agent': 'WISP-JS Discord Bot | Client'
+
+    }
+
+});
+
+exports.listener = async function(args) { // Expects args to be Array()
+    try {
+        let result;
+        switch(args[1]) {
+            case 'status': case 'players':
+                const response = await instance.get(`${config.servers[args[0]]}/utilization`);
+                result = await embed(args[1], response)
+                return result;
+            break;
+            case 'cmd': case 'power':
+                let type = "command"
+                let signal = type;
+                let data = args.slice(2,args.length).join(" ");
+                if(args[1] == "power") {
+                    type = args[1];
+                    signal = "signal";
+                }
+                await instance.post(
+                    `${config.servers[args[0]]}/${type}`, 
+                    {[signal]: data}
+                );
+                result = await embed(args[1], data)
+                return result;
+            break;
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
