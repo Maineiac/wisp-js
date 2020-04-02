4316712f624b90c952eeb621b857d81e22cc9461
Maineiac <cfaulkingham@hotmail.com>
2020-04-02 12:53:23 -0400

    use new config variables

--- bin/client/embed.js
+++ bin/client/embed.js
@@ -7,36 +7,36 @@ module.exports = async function(type, data=null) {
     switch(type) {
         case 'status':
             prop = await format.ServerStatus(data);
-            embed.setAuthor("Server Status", config.icons.status)
+            embed.setAuthor(config.embeds.status.title, config.embeds.status.icon)
         break;
         
         case 'players':
             prop = await format.PlayerList(data);
-            embed.setAuthor("Player List", config.icons.players)
+            embed.setAuthor(config.embeds.players.title, config.embeds.players.icon)
         break;
 
         case 'cmd':
             prop.name = "Success";
-            prop.color = 53611;
+            prop.color = config.embeds.cmd.color.success;
             prop.desc = "Sent command : `"+data+"`";
-            embed.setAuthor("Remote Console", config.icons.cmd);
+            embed.setAuthor(config.embeds.cmd.title, config.embeds.cmd.icon);
         break;
 
         case 'power':
             prop.name = "Success";
-            prop.color = 53611;
+            prop.color = config.embeds.power.color.success;
             prop.desc = "Sent signal : `"+data+"`";
-            embed.setAuthor("Remote Control", config.icons.power);
+            embed.setAuthor(config.embeds.power.title, config.embeds.power.icon);
         break;
 
         case 'servers':
             prop = await format.ServerList(config.servers);
-            embed.setAuthor("Server List", config.icons.serverlist);
+            embed.setAuthor(config.embeds.serverlist.title, config.embeds.serverlist.icon);
         break;
 
         case 'help':
             prop = await format.ClientHelp(config.prefix);
-            embed.setAuthor("Server List", config.icons.help);
+            embed.setAuthor(config.embeds.help.title, config.embeds.help.icon);
         break;
     }
     embed.setTitle(prop.name)
