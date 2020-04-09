172592fe6f108a694e8b3b7d0b8c91d7c5c77ead
Maineiac <cfaulkingham@hotmail.com>
2020-04-09 13:11:51 -0400

    Implement permissions

--- src/client/cmds.js
+++ src/client/cmds.js
@@ -14,7 +14,7 @@ const cmds = {
     }
 }
 
-module.exports = async function (args) { // Expects args to be Array()
+module.exports = async function (args, roles) { // Expects args to be Array()
     let time = Date.now();
     let result;
 
@@ -25,16 +25,24 @@ module.exports = async function (args) { // Expects args to be Array()
         );
 
     } else if(_.isFunction(cmds.alias[args[1]]) && config.servers.hasOwnProperty(args[0]))  {
-        result = await embed(
-            await cmds.alias[args[1]](args)
-        );
+
+        if(roles.some(role => config.permissions[args[1]].includes(role))
+        || config.permissions[args[1]].includes('*')) {
+
+            result = await embed(
+                await cmds.alias[args[1]](args)
+            );
+
+        } else {
+            result = `You don't have permission to use \`${args[1]}\``;
+        }
 
     } else if(!_.isFunction(cmds.alias[args[1]]) && config.servers.hasOwnProperty(args[0]) ){
         result = `Invalid alias sub-command : \`${args[1]}\``;
 
     } else {
 
-        result = `Invalid command : \`${args[1]}\``;
+        result = `Invalid command : \`${args[0]}\``;
 
     }
     let end = Date.now() - time;
