97e9d6b60e30f5612a0465677a2dc3ba19631df7
Maineiac <cfaulkingham@hotmail.com>
2020-04-09 13:12:18 -0400

    pass roles to cmd

--- src/listener.js
+++ src/listener.js
@@ -1,11 +1,11 @@
 const config = require('../config');
 
-module.exports = async function(msg, roles) {
+module.exports = async function(msg) {
     if(msg.content.startsWith(config.prefix) && !msg.author.bot) { 
         try {
             let args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
             let result;
-            result = await require('./client/cmds')(args);
+            result = await require('./client/cmds')(args, msg.member.roles.member._roles);
             if(!result) { result = "There was an error communicating with the server. Please check your config." }
             msg.channel.send(result);
         } catch(error) {
