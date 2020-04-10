d7b7e7ff9e16fe333831a0abf2eff4249b15b771
Maineiac <cfaulkingham@hotmail.com>
2020-04-09 22:53:32 -0400

    Change how commands are called to new method

--- src/admin/base_cmds.js
+++ src/admin/base_cmds.js
@@ -1,52 +1,35 @@
-const config = require('../../config.js');
-const user = require('./cmds/user.js');
-const node = require('./cmds/node.js');
-const location = require('./cmds/location.js');
-const server = require('./cmds/server.js');
-const nest = require('./cmds/nest.js');
+const _ = require('underscore');
 
+const config = require('../../config');
+const embed = require('./embed');
 
-// 
-const instance = require('axios').create({
+const cmds = {
+    location: require('./cmds/locations'),
+    nest: require('./cmds/nests'),
+    node: require('./cmds/nodes'),
+    server: require('./cmds/servers'),
+    user: require('./cmds/users')
 
-    /*  This is all information that is used to make get/post resquests
-        from/to the WISP API. The documentation can be found here :
-            https://docs.panel.gg/#introduction
-        This configures axios one time to use the information to make requests.
-    */
-    baseURL: `${config.PanelURL}api/application/`,
-    timeout: 5000,
-    headers: {
+}
 
-        'Content-Type': 'application/json',
-        'Accept': 'application/vnd.wisp.v1+json',
-        'Authorization': `Bearer ${config.WISPACPAPIKey}`,
-        'User-Agent': 'WISP-JS Discord Bot | Admin'
+module.exports = async function (args) { // Expects args to be Array()
+    let time = Date.now();
+    let result;
+
+    if(_.isFunction(cmds[args[0]])) {
+
+        result = await embed(
+            await cmds[args[0]](args)
+        );
+
+    } else {
+
+        result = `Invalid command : \`${args[0]}\``;
 
     }
-
-});
-
-
-
-exports.listener = async function(args) { // Expects args to be Array()
-    switch(args[0]) {
-        case 'user':
-            result = await user(args);
-            return result;
-        case 'node':
-            result = await node(args);
-            return result;
-        case 'location':
-            result = await location(args);
-            return result;
-        case 'server':
-            result = await server(args);
-            return result;
-        case 'nest':
-            result = await nest(args);
-            return result;
-        default:
-            return 'Unknown command';
+    let end = Date.now() - time;
+    if(config.debug) {
+        console.log(`Result took ${end}ms`);
     }
+    return result;
 }
