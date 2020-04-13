15f8979618e39fd524363c1b2641083c0d09ab35
Maineiac <cfaulkingham@hotmail.com>
2020-04-13 08:10:24 -0400

    catch errors for !node get

--- src/admin/cmds/node/get.js
+++ src/admin/cmds/node/get.js
@@ -13,18 +13,22 @@ module.exports = async function(args) {
             icon: config.embeds.footer.icon
         }
     };
-    const data = await request.get(`/nodes/${args[2]}`);
-    const created = new Date(data.attributes.created_at).toDateString();
-    const array = [
-        [`Name:`, data.attributes.name],
-        [`Node ID:`, data.attributes.id],
-        [`Location ID:`, data.attributes.location_id],
-        [`Memory:`, data.attributes.memory],
-        [`Disk:`, data.attributes.disk],
-        [`CPU:`, data.attributes.cpu],
-        [`Created:`, created]
-        
-    ]
-    obj.desc = `${data.attributes.description}\`\`\`${table(array, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
-    return obj;
+    try {
+        const data = await request.get(`/nodes/${args[2]}`);
+        const created = new Date(data.attributes.created_at).toDateString();
+        const array = [
+            [`Name:`, data.attributes.name],
+            [`Node ID:`, data.attributes.id],
+            [`Location ID:`, data.attributes.location_id],
+            [`Memory:`, data.attributes.memory],
+            [`Disk:`, data.attributes.disk],
+            [`CPU:`, data.attributes.cpu],
+            [`Created:`, created]
+            
+        ]
+        obj.desc = `${data.attributes.description}\`\`\`${table(array, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
+        return obj;
+    } catch(error) {
+        return errors(error, 'admin/node/edit.js : line 51');
+    }
 }
