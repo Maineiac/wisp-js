dfdcdf45a8d13d0797ad1e3225a632f622eda5d9
Maineiac <cfaulkingham@hotmail.com>
2020-04-13 13:19:08 -0400

    Create !user list

---
+++ src/admin/cmds/user/list.js
@@ -0,0 +1,36 @@
+const request = require('../../request');
+const config = require('../../../../config');
+const table = require('text-table');
+module.exports = async function() {
+
+    let obj = {
+        title: {
+            text: "User List",
+            icon: config.embeds.servers.icon
+        },
+        color: config.embeds.servers.color,
+        footer: {
+            text: config.embeds.footer.text,
+            icon: config.embeds.footer.icon
+        }
+    };
+    let array = [["ID", "Username", "Type"]];
+    const data = await request.get('/users');
+    const users = data.data;
+    for(i = 0; i < users.length; i++) {
+        array[i+1] = [
+
+            users[i].attributes.id,
+
+            (users[i].attributes.last_name) ? 
+                `${users[i].attributes.first_name} ${users[i].attributes.last_name}` : 
+                users[i].attributes.first_name,
+                
+            (users[i].attributes.root_admin) ? "Admin" : "User"
+        ]
+    }
+    console.log(users[3])
+    obj.desc = '```json\n'+table(array, { align: [ 'r', 'c', 'l' ], hsep: [ '     ' ] })+'```';
+    return obj;
+
+}
