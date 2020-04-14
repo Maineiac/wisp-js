03fdecb94b72b77eb50571177f18635da9a09844
Maineiac <cfaulkingham@hotmail.com>
2020-04-14 10:55:01 -0400

    Create !user create [params]

---
+++ src/admin/cmds/user/create.js
@@ -0,0 +1,52 @@
+
+const request = require('../../request');
+const config = require('../../../../config');
+const errors = require('../../error');
+const util = require('../../../util.js');
+const _ = require('underscore');
+const table = require('text-table');
+module.exports = async function(args) {
+
+    let obj = {
+        title: {
+            text: `User Creator`,
+            icon: config.embeds.servers.icon
+        },
+        color: config.embeds.servers.color,
+        desc: "",
+        footer: {
+            text: config.embeds.footer.text,
+            icon: config.embeds.footer.icon
+        }
+    };
+
+    const params = await util.parseRawParams(
+
+            _.compact(
+            util.parseParamsWithQuotes(
+                args.slice(2).join(" "))
+        )
+    );
+
+    try {
+        
+        const data = await request.post(`/users`, params);
+
+
+        const array = util.arrayBooleansToStrings(
+
+            [[`Property`, `Value`]].concat(
+                Object.entries(data.data.attributes)
+            )
+
+        );
+
+        obj.desc = `Sent data to panel.\n\`\`\`${table(array, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
+        
+        return obj;
+
+    } catch(error) {
+        return errors(error, 'admin/user/create.js : line 54');
+
+    }
+}
