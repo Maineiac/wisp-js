03943fec265e81eae490d86c5e973985c1b8e318
Maineiac <cfaulkingham@hotmail.com>
2020-04-14 07:20:47 -0400

    Fix bug in embed.js

--- src/admin/embed.js
+++ src/admin/embed.js
@@ -1,12 +1,29 @@
 const Discord = require("discord.js");
-const config = require('../../config.js');
-module.exports = async function(prop) {
-    const embed = new Discord.MessageEmbed();
-    if (prop.title) { embed.setAuthor(prop.title.text, prop.title.icon) }
-    if (prop.head) { embed.setTitle(prop.head) }
-    if (prop.color) { embed.setColor(prop.color) }
-    if (prop.footer) { embed.setFooter(prop.footer.text, prop.footer.icon) }
-    if (prop.desc) { embed.setDescription(prop.desc) }
-    embed.setTimestamp();
-    return embed;
+
+module.exports = async function (prop) {
+    
+    if (prop) {
+        const embed = new Discord.MessageEmbed();
+        
+        embed.setAuthor(
+            (prop.title.text) ? prop.title.text : "??", 
+            (prop.title.icon) ? prop.title.icon : ""
+        );
+
+        embed.setFooter(
+            (prop.footer.text) ? prop.footer.text : "??", 
+            (prop.footer.icon) ? prop.footer.icon : "")
+        ;
+
+        embed.setTitle((prop.head) ? prop.head : "");
+        embed.setColor((prop.color) ? prop.color : "");
+        embed.setDescription((prop.desc) ? prop.desc : "It seems odd there is nothing here...")
+        embed.setTimestamp();
+
+        return embed;
+
+    } else {
+        return "Something really bad happened...";
+
+    }
 } 
