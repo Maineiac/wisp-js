2d24e1129d627fa560109b2ed44ce598fefb00b8
Maineiac <cfaulkingham@hotmail.com>
2020-04-14 18:42:49 -0400

    Create !nest get (id)

---
+++ src/admin/cmds/nest/get.js
@@ -0,0 +1,36 @@
+const request = require('../../request');
+const config = require('../../../../config');
+const errors = require('../../error');
+const util = require('../../../util.js');
+const _ = require('underscore');
+const table = require('text-table');
+module.exports = async function(args) {
+    let obj = {
+        title: {
+            text: "Nest Lookup",
+            icon: config.embeds.servers.icon
+        },
+        color: config.embeds.servers.color,
+        footer: {
+            text: config.embeds.footer.text,
+            icon: config.embeds.footer.icon
+        }
+    };
+    try {
+        const data = await request.get(`/nests/${args[2]}`);
+        const created = new Date(data.attributes.created_at).toDateString();
+        const array = [
+            [`ID`, data.attributes.id],
+            [`Name`, data.attributes.name],
+            [`Author`, data.attributes.author],
+            [`Created`, created],
+            [`UUID`, data.attributes.uuid]
+            
+        ]
+        obj.desc = `${data.attributes.description}
+        \`\`\`${table(array, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
+        return obj;
+    } catch(error) {
+        return errors(error, 'admin/nests/get.js : line 20');
+    }
+}
