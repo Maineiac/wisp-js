5bb3e780eb09cf9f602eb64eb2dc11467cb14fb4
Maineiac <cfaulkingham@hotmail.com>
2020-04-16 23:36:19 -0400

    Create !server get limits (id)

---
+++ src/admin/cmds/server/get/limits.js
@@ -0,0 +1,50 @@
+const request = require(`${process.env.root}/src/admin/request`);
+const config = require(`${process.env.root}/config`);
+const errors = require(`${process.env.root}/src/admin/error`);
+const util = require(`${process.env.root}/src/util.js`);
+const _ = require('underscore');
+const table = require('text-table');
+module.exports = async function(args) {
+    let id = args[3];
+    let obj = {
+        title: {
+            text: "Server Lookup",
+            icon: config.embeds.servers.icon
+        },
+        color: config.embeds.servers.color,
+        footer: {
+            text: config.embeds.footer.text,
+            icon: config.embeds.footer.icon
+        }
+    };
+    if(isNaN(args[3])) {
+        const servers = await request.getRecursive(`/servers`);
+        for(const s of servers) {
+            console.log(s.attributes.identifier)
+            if(args[3] == s.attributes.identifier) {
+                id = s.attributes.id;
+            }
+        }
+    }
+    try {
+        const data = await request.get(`/servers/${id}`);
+        const created = new Date(data.attributes.created_at).toDateString();
+        const limits = [
+            [`     Memory`, data.attributes.limits.memory],
+            [`Swap`, data.attributes.limits.swap],
+            [`Disk`, data.attributes.limits.disk],
+            [`IO`, data.attributes.limits.io],
+            [`CPU`, data.attributes.limits.cpu]
+        ];
+
+        const feature_limits = [
+            [`Databases`, data.attributes.feature_limits.databases],
+            [`Allocations`, data.attributes.feature_limits.allocations]
+        ]
+            
+        obj.desc = `**#${data.attributes.id}** | \`${data.attributes.identifier}\`\n\`\`\`\nHardware\n${table(limits, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\n\nFeatures\n${table(feature_limits, { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
+        return obj;
+    } catch(error) {
+        return errors(error, 'admin/user/get.js : line 51');
+    }
+}
