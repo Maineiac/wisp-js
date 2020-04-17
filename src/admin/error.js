2270438eaf09cc69c62c9ca6e78b89b880d7dbd7
Maineiac <cfaulkingham@hotmail.com>
2020-04-17 13:16:29 -0400

    Rewrite error handling

--- src/admin/error.js
+++ src/admin/error.js
@@ -1,35 +1,52 @@
 const config = require(`${process.env.root}/config`);
 const errors = {
-    400: ['**400** | `Bad Request`', 'Probably a bad config.'],
-    401: ['**401** | `Unauthorized`', 'The Dino API key is probably invalid.'],
-    403: ['**403** | `Forbidden`', 'Invalid `WISPAPIKey` in config.js'],
-    404: ['**404** | `Not Found`', 'The url used for the request was invalid.\n\nThis can happen because :\n • The id/name provided for the node/server/allocation/egg/nest/location/user is invalid.\n • Someone tampered with request.js\n • Someone tampered with the command calling request.js'],
-    405: ['**405** | `Method Not Allowed`', 'This shouldn\'t happen, contact Maineiac#0001.'],
-    412: ['**412** | `Precondition Failed`', 'The server is probably not running.'],
-    429: ['**429** | `Too Many Requests`', 'Your bot has tried to use the api too many times recently, it has been temporarily rate limited'],
-    500: ['**500** | `Internal Server Error`', 'WISP has had a problem. Contact support.'],
-    503: ['**503** | `Service Unavailable`', 'WISP is temporarily offline for maintenance, try again later.'],
-    504: ['**504** | `DaemonConnectionException`', 'The daemon isn\'t responding. '],
-    'ENOTFOUND': ['**ENOTFOUND**', 'Invalid PanelURL in config.js, no instance to make requests from.']
+    400: ['Probably a bad config.'],
+    401: ['The Dino API key is probably invalid.'],
+    403: ['Invalid `WISPAPIKey` in config.js'],
+    404: ['The url used for the request was invalid.\n\nThis can happen because :\n • The id/name provided for the node/server/allocation/egg/nest/location/user is invalid.\n • Someone tampered with request.js\n • Someone tampered with the command calling request.js'],
+    405: ['This shouldn\'t happen, contact Maineiac#0001.'],
+    412: ['The server is probably not running.'],
+    422: ['Missing required parameters. Hopefully I print the fields missing below.'],
+    429: ['Your bot has tried to use the api too many times recently, it has been temporarily rate limited'],
+    500: ['WISP has had a problem. Contact support.'],
+    503: ['WISP is temporarily offline for maintenance, try again later.'],
+    504: ['The daemon isn\'t responding. '],
+    'ENOTFOUND': ['Invalid PanelURL in config.js, no instance to make requests from.']
 }
-module.exports = function(error, location) {
+module.exports = function (error, location) {
     console.log(error);
-    const handled = errors[
-        (error.response) ? error.response.status : error.code
+
+    const handled = [
+        (error.response) ? `**${error.response.status}** | \`${error.response.statusText}\`` : error.code,
+        errors[(error.response) ? error.response.status : error.code]
     ];
+
     const obj = {
         title: {
             text: config.embeds.error.title,
             icon: config.embeds.error.icon
-        },  
-        color: config.embeds.error.color, 
-        desc: `${handled[0]}\n\n${handled[1]}${(config.debug) ? `\n\n\`\`\`${location}\`\`\`` : ''}`,
+        },
+        color: config.embeds.error.color,
+        desc: `There was an error that I don't know how to handle.`,
         footer: {
             icon: config.embeds.footer.icon,
             text: config.embeds.footer.text
         }
     }
-    if(config.debug) {
+    if (handled) {
+        obj.desc = `${handled[0]}\n\n${handled[1]}${(config.debug) ? `\n\n\`\`\`${location}\`\`\`` : ''}`;
+    } else {
+        obj.desc = "Unhandled";
+    }
+    if(error.response.data.errors) {
+        let build = "";
+        for(const e of error.response.data.errors) {
+            build = `${build}${e.detail}\n`;
+        }
+        obj.desc = `${obj.desc}\n\`\`\`\n${build}\`\`\``;
+    }
+
+    if (config.debug) {
         console.log(`Parsed error : ${(error.response) ? error.response.status : error.code} | in : ${location}`);
     }
     return obj;
