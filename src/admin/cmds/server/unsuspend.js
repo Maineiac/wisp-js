a31e2302a32b2791a6a58c09543675445f561151
Maineiac <cfaulkingham@hotmail.com>
2020-04-17 13:51:38 -0400

    Create !server unsuspend (id,ids)

---
+++ src/admin/cmds/server/unsuspend.js
@@ -0,0 +1,27 @@
+
+const request = require(`${process.env.root}/src/admin/request`);
+const config = require(`${process.env.root}/config`);
+const errors = require(`${process.env.root}/src/admin/error`);
+const util = require(`${process.env.root}/src/util.js`);
+const _ = require('underscore');
+const table = require('text-table');
+
+module.exports = async function(args) {
+    let obj = util.baseEmbedObj('servers', "Server Editor");
+
+    const ids = args[2].split(`,`);
+
+    for(const id of ids) {
+
+        try {
+            await request.post(`/servers/${id}/unsuspend`);
+            obj.desc = `${obj.desc}\nUnsuspended server #${id}`;
+
+        } catch(error) {
+            return errors(error, 'admin/server/unsuspend.js : line 17');
+
+        }
+    }
+    
+    return obj;
+}
