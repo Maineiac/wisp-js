4e1a6aa052a0e8230af02a9fcfc8327b8d845924
Maineiac <cfaulkingham@hotmail.com>
2020-04-21 15:41:16 -0400

    More command structure fixes

--- src/admin/cmds/nest/egg/list.js
+++ src/admin/cmds/nest/egg/list.js
@@ -6,9 +6,10 @@ const _ = require('underscore');
 const table = require('text-table');
 
 module.exports = async function(args) {
+
     let obj = {
         title: {
-            text: `Egg Lookup`,
+            text:`Egg List | Nest ${args[3]}`,
             icon: config.embeds.servers.icon
         },
         color: config.embeds.servers.color,
@@ -17,23 +18,33 @@ module.exports = async function(args) {
             icon: config.embeds.footer.icon
         }
     };
+
+    let data;
+    let array = [["ID", "Name"]];
+
     try {
-        const data = await request.get(`/nests/${args[3]}/eggs/${args[4]}`);
-        const created = new Date(data.attributes.created_at).toDateString();
-        const array = [
-            [`ID`, data.attributes.id],
-            [`Nest`, data.attributes.nest],
-            [`Name`, data.attributes.name],
-            [`Author`, data.attributes.author],
-            [`Created`, created],
-            [`Image`, data.attributes.docker_image]
-            [`UUID`, data.attributes.uuid]
-            
-        ]
-        obj.desc = `${data.attributes.description}
-        \`\`\`${table(util.cleanArray(array), { align: [ 'r', 'l'], hsep: [ '   ' ] })}\`\`\``;
-        return obj;
+        data = await request.get(`/nests/${args[3]}/eggs`);
+        console.log(data.data[0]);
+
     } catch(error) {
-        return errors(error, 'admin/nests/get/egg.js : line 21');
+        return errors(error, 'admin/allocation/list.js : line 26');
+
     }
+
+    const eggs = data.data;
+
+    for(i = 0; i < eggs.length; i++) {
+
+        array[i+1] = [
+
+            eggs[i].attributes.id,
+            eggs[i].attributes.name
+
+        ]
+    }
+
+    obj.desc = `\`\`\`json\n${table(array, { align: [ 'c', 'c', 'c' ], hsep: [ '   ' ] })}\`\`\``;
+    
+    
+return obj;
 }
