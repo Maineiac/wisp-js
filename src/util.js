8463dae3431f6e7f7adc20b862d73ff044c4abb5
Maineiac <cfaulkingham@hotmail.com>
2020-04-20 23:06:09 -0400

    null checks for param parser

--- src/util.js
+++ src/util.js
@@ -4,49 +4,52 @@ const _ = require('underscore');
 module.exports.parseParamsWithQuotes = function(args) {
 
     let params = args.match(/[^\s"']+|"([^"]*)"/gmi);
-    
-    for(i = 0; i < params.length; i++){
+    if(params) {
+        for(i = 0; i < params.length; i++){
 
-        if(params[i] != null && 
-            params[i+1] != null && 
-            params[i].charAt(params[i].length - 1) == "=") {
+            if(params[i] != null && 
+                params[i+1] != null && 
+                params[i].charAt(params[i].length - 1) == "=") {
 
-            //Move strings wrapped in "" to the correct key
-            if(params[i+1].startsWith('"') || params[i+1].startsWith("'")) {
-                params[i] = `${params[i]}${params[i+1]}`; // Looking for better ways to do this
-                params[i+1] = null; // After a bunch of checks, this magic reformats our array
-            
+                //Move strings wrapped in "" to the correct key
+                if(params[i+1].startsWith('"') || params[i+1].startsWith("'")) {
+                    params[i] = `${params[i]}${params[i+1]}`; // Looking for better ways to do this
+                    params[i+1] = null; // After a bunch of checks, this magic reformats our array
+                
+                }
+
             }
 
         }
-
+        return params;
     }
-    return params;
+    return null;
 }
 
 module.exports.parseRawParams = function(params, base=false) {
 
     let newParams = (base) ? base : {};
+    if(params) {
+        for(const p of params) {
+            let arr = p.split("=");
+            let value = arr[1].replace(/"/g, '');
+            value = value.replace(/'/g, '');
 
-    for(const p of params) {
-        let arr = p.split("=");
-        let value = arr[1].replace(/"/g, '');
-        value = value.replace(/'/g, '');
+            if(value == "true") {
+                value = true;
+            } else if(value == "false") {
+                value = false;
+            } 
 
-        if(value == "true") {
-            value = true;
-        } else if(value == "false") {
-            value = false;
-        } 
+            let dot = arr[0].split(".")
+            if(dot[1]) {
+                newParams[dot[0]] = (newParams[dot[0]]) ? newParams[dot[0]] : {};
+                newParams[dot[0]][dot[1]] = value;
+            } else {
+                newParams[arr[0]] =  value;
+            }
 
-        let dot = arr[0].split(".")
-        if(dot[1]) {
-            newParams[dot[0]] = (newParams[dot[0]]) ? newParams[dot[0]] : {};
-            newParams[dot[0]][dot[1]] = value;
-        } else {
-            newParams[arr[0]] =  value;
         }
-
     }
 
     if(newParams.ports) {
@@ -59,7 +62,7 @@ module.exports.cleanArray = function(array) {
     let parsed = [];
 
     for(const p in array) {
-        if(array[p][0] != "STARTUP") {
+        if(array[p] && array[p][0] != "STARTUP") {
             parsed[p] = [
 
                 array[p][0],
