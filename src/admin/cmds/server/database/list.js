faa3f2a05b89f8de4d704a4ca7aa21b4af8260d8
Maineiac <cfaulkingham@hotmail.com>
2020-04-21 00:01:47 -0400

    Create `!server database list (id)`

--- src/admin/cmds/server/database/list.js
+++ src/admin/cmds/server/database/list.js
@@ -8,7 +8,7 @@ module.exports = async function(args) {
 
     let obj = {
         title: {
-            text:`Server List`,
+            text:`Database list`,
             icon: config.embeds.servers.icon
         },
         color: config.embeds.servers.color,
@@ -20,32 +20,31 @@ module.exports = async function(args) {
 
     const page = (args[4]) ? args[4] : 1;
     let data;
-    let array = [["ID", "Identifier", "Name"]];
+    let array = [["ID", "Name"]];
 
     try {
-        data = await request.get(`/servers?page=${page}`);
-        console.log(data.data[0]);
+        data = await request.get(`/servers/${args[3]}/databases`);
+        console.log(data.data);
 
     } catch(error) {
-        return errors(error, 'admin/server/list.js : line 26');
+        return errors(error, 'admin/list/database.js : line 26');
 
     }
 
-    const servers = data.data;
+    const databases = data.data;
 
-    for(i = 0; i < servers.length; i++) {
+    for(i = 0; i < databases.length; i++) {
 
         array[i+1] = [
 
-            servers[i].attributes.id,
-            servers[i].attributes.identifier,
-            servers[i].attributes.name
+            databases[i].attributes.id,
+            databases[i].attributes.database
 
         ]
     }
 
-    obj.desc = `Page ${page}/${data.meta.pagination.total_pages}\n
-    \`\`\`${table(array, { align: [ 'c', 'c', 'c' ], hsep: [ '   ' ] })}\`\`\``;
+    obj.desc = `\n
+    \`\`\`${table(array, { align: [ 'c', 'l' ], hsep: [ '   ' ] })}\`\`\``;
     
     
 return obj;
