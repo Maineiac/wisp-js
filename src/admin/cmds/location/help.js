5175b74a44656c38221df728aeed2b3ad1fd4ddc
Maineiac <cfaulkingham@hotmail.com>
2020-04-22 12:14:18 -0400

    Bugfixes

--- src/admin/cmds/location/help.js
+++ src/admin/cmds/location/help.js
@@ -10,7 +10,7 @@ module.exports = async function() {
 \`[] = optional\`\n
 \`\`\`dsconfig\n${p}location list\`\`\`
 \`\`\`dsconfig\n${p}location get (locationid)\`\`\`Example : \`!location get 3\`\n
-\`\`\`dsconfig\n${p}location delete (locationid/ids)\`\`\`Example : \`!location delete 3,6,9"\`\n
+\`\`\`dsconfig\n${p}location delete (locationid/ids)\`\`\`Example : \`!location delete 3,6,9\`\n
 \`\`\`dsconfig\n${p}location edit (parameters)\`\`\`Example : \`!location edit short=new-loc long="New location name"\`\n
 \`\`\`dsconfig\n${p}location create (parameters)\`\`\`Required Parameters : *short*, *long*
 Example : \`!location create short=ca-mnt long="Montreal, Canada"\`\n
