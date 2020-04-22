480235f9667f45142e5d5135711d0fa9e20868f2
Maineiac <cfaulkingham@hotmail.com>
2020-04-22 02:00:50 -0400

    renamed client cmd parser

---
+++ src/client/parser.js
@@ -0,0 +1,53 @@
+const _ = require('underscore');
+
+const settings = require(`${process.env.root}/config/settings`);
+const aliases = require(`${process.env.root}/config/aliases`);
+const permissions = require(`${process.env.root}/config/permissions/client`);
+const embed = require(`${process.env.root}/src/client/embed`);
+
+const r = `${process.env.root}/src/client/cmds/`;
+
+const cmds = {
+    help: require(`${r}help`),
+    servers: require(`${r}servers`),
+    alias: {
+        status: require(`${r}status`),
+        players: require(`${r}players`),
+        power: require(`${r}rcon`),
+        cmd: require(`${r}rcon`)
+    }
+}
+
+module.exports = async function (args, roles) { // Expects args to be Array()
+    let time = Date.now();
+    let result;
+
+    if(_.isFunction(cmds[args[0]])) {
+
+        result = await embed(
+            await cmds[args[0]]()
+        );
+        
+    } else if(_.isFunction(cmds.alias[args[1]]) && aliases.hasOwnProperty(args[0]))  {
+
+        if(roles.some(role => permissions[args[1]].includes(role))
+        || permissions[args[1]].includes('*')) {
+
+            result = await embed(
+                await cmds.alias[args[1]](args)
+            );
+
+        } else {
+            result = `You don't have permission to use \`${args[1]}\``;
+        }
+
+    } else if(!_.isFunction(cmds.alias[args[1]]) && aliases.hasOwnProperty(args[0]) ){
+        result = `Invalid alias sub-command : \`${args[1]}\``;
+
+    }
+    let end = Date.now() - time;
+    if(settings.debug) {
+        console.log(`Client: Result took ${end}ms`);
+    }
+    return result;
+}
