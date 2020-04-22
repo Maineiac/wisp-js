0632e37ca075133f3d9f4d09c343b390f431d716
Maineiac <cfaulkingham@hotmail.com>
2020-04-22 00:59:54 -0400

    bugfixes

--- src/admin/cmds/server/create.js
+++ src/admin/cmds/server/create.js
@@ -1,13 +1,12 @@
 
 const request = require(`${process.env.root}/src/admin/request`);
-const config = require(`${process.env.root}/config`);
 const errors = require(`${process.env.root}/src/admin/error`);
 const util = require(`${process.env.root}/src/util.js`);
 
 const fs = require('fs');
 const _ = require('underscore');
 
-const getServer = require('./get/get')
+const getServer = require('./get/all')
 
 module.exports = async function(args) {
 
