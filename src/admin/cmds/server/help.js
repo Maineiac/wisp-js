b9d5f7faf8ff60f73286be2f1780c0ed88835ec8
Maineiac <cfaulkingham@hotmail.com>
2020-04-23 10:07:19 -0400

    Add clone to `!server help`

--- src/admin/cmds/server/help.js
+++ src/admin/cmds/server/help.js
@@ -13,12 +13,13 @@ The server command is the most complicated command I have.\n
 \`\`\`dsconfig\n${p}server get help\`\`\`This sub-command has it's own help menu.
 \`\`\`dsconfig\n${p}server edit help\`\`\`This sub-command has it's own help menu.
 \`\`\`dsconfig\n${p}server create help\`\`\`This sub-command has it's own help menu.
+\`\`\`dsconfig\n${p}server clone (id) (allocationid)\`\`\`Example : \`!server clone 54 357\`\n
+\`\`\`dsconfig\n${p}server save (id) (name)\`\`\`Example : \`!server save 3 gmod\`\n
 \`\`\`dsconfig\n${p}server reinstall (serverid/ids)\`\`\`Example : \`!server reinstall 3,6,9\`\n
 \`\`\`dsconfig\n${p}server rebuild (serverid/ids)\`\`\`Example : \`!server rebuild 3,6,9\`\n
 \`\`\`dsconfig\n${p}server suspend (serverid/ids)\`\`\`Example : \`!server suspend 3,6,9\`\n
 \`\`\`dsconfig\n${p}server unsuspend (serverid/ids)\`\`\`Example : \`!server unsuspend 3,6,9\`\n
 \`\`\`dsconfig\n${p}server delete (serverid/ids)\`\`\`Example : \`!server delete 3,6,9\`\n
-\`\`\`dsconfig\n${p}server save (id) (name)\`\`\`Example : \`!server save 3 gmod"\`\n
 `
 
     return obj;
